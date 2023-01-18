2f9d61e3ec342a011cb8a95d9dd782459301fb98
Spidw3b <jarod64@hotmail.fr>
2023-01-18 11:48:13 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddContent(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Add("Nom : " + textBoxNom.Text);
+            listBox.Items.Add("Prénom : "+textBoxPrenom.Text);
+            listBox.Items.Add("Date de naissance : " + dateDeNaissance.Text);
+        }
+    }
+}
